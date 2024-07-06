890c153e43c405377a3dfb0cf3895c84fbd13e41
Diesos <ozturk@live.Fr>
2024-07-06 15:26:32 +0200

    feat: Add CRUD operations for children in backend

--- backend/controllers/childrenController.js
+++ backend/controllers/childrenController.js
@@ -18,13 +18,16 @@ const getAllChildren = async (req, res) => {
 	res.status(500).json({ error: error.message });
   }
 };
-
+// Find a child by id
 const getChild = async (req, res) => {
 	const { id } = req.params;
 	try {
 	  const child = await Enfant.findOne({ where: { id } });
 	  if (!child) {
-		return res.status(404).json({ error: "Child not found" });
+		return res.status(404).json({
+			succes: false,
+			error: "Child not found"
+			});
 	  }
 	  res.status(200).send({
 		succes: true,
@@ -37,7 +40,51 @@ const getChild = async (req, res) => {
 			message: "Child not found",
 			error: error.message });
 	}
-}
+};
+// Add a child
+const addChild = async (req, res) => {
+	const { prenom, nom, date_naissance, allergie, diet} = req.body;
+	try {
+	  const child = await Enfant.create({
+		prenom,
+		nom,
+		date_naissance,
+		allergie,
+		diet
+	  });
+	  res.status(201).send({
+		succes: true,
+		message: "Child created",
+		data: child
+	  });
+	} catch (error) {
+	  res.status(500).json({ error: error.message });
+	}
+  }
+
+const updateChild = async (req, res) => {
+	const { id } = req.params;
+	const { prenom, nom, date_naissance, allergie, diet } = req.body;
+	try {
+	  const child = await Enfant.findOne({ where: { id } });
+	  if (!child) {
+		return res.status(404).json({ error: "Child not found" });
+	  }
+	  child.prenom = prenom;
+	  child.nom = nom;
+	  child.date_naissance = date_naissance;
+	  child.allergie = allergie;
+	  child.diet = diet;
+	  await child.save();
+	  res.status(200).send({
+		succes: true,
+		message: "Child updated",
+		data: child
+	  });
+	} catch (error) {
+	  res.status(500).json({ error: error.message });
+	}
+  }
 
 
-module.exports = { getAllChildren, getChild };
+module.exports = { getAllChildren, getChild, addChild, updateChild};
--- backend/routes/childrenRoutes.js
+++ backend/routes/childrenRoutes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { getAllChildren } = require('../controllers/childrenController');
-const { getChild } = require('../controllers/childrenController');
+const {
+	getAllChildren,
+	getChild,
+	addChild,
+	updateChild } = require('../controllers/childrenController');
 
+// GET ROUTES for children
+// GET all children
 router.get('/getallchildren', getAllChildren);
+// GET a child
 router.get('/getchild/:id', getChild);
+
+
+// POST ROUTES for children
+// POST a child
+router.post('/addchild', addChild);
+
+//PUT ROUTES for children
+//PUT a child
+router.put('/updatechild/:id', updateChild);
 
 module.exports = router;
